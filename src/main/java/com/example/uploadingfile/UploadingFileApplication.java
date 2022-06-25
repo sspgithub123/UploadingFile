b03b2aaddee4c62183176acb4826d74d7de83fa3
Shubham <ssp671995@gmail.com>
2022-06-25 12:46:14 +0530

    Added the uploading file program.

---
+++ src/main/java/com/example/uploadingfile/UploadingFileApplication.java
@@ -0,0 +1,14 @@
+package com.example.uploadingfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFileApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
